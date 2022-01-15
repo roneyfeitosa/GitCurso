43cdf185436d40d680ed9b11267609ea926d894f
roneyfeitosa <roneyfeitosa@gmail.com>
2022-01-15 14:07:38 -0300

    Adicionado Roney

---
+++ CidadesController.cs
@@ -0,0 +1,32 @@
+﻿using CRUDVacinas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CRUDVacinas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CidadesController : ControllerBase
+    {
+        //[HttpGet("")]
+        //public IActionResult Get()
+        //{
+        //    return new Cidade(){ Nome = "Recife" });
+        //}
+
+        /*
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            return Ok(new Cidade());
+
+        }
+        [Route("api/[controller]")]
+        */
+    }
+}
